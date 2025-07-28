ad26f9f37a7681af5eae19ff068d58bcfe1823ca
Gentrithotii <Gentrithoti@hotmail.com>
2025-07-28 17:00:46 +0200

    Tested for abit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  function addToCount() {
+    setCount(count + 1);
+  }
+
+  function removeFromCount() {
+    if (count <= 0) {
+      console.log("Cant be less than 0");
+    } else {
+      setCount(count - 1);
+    }
+  }
 
   return (
     <>
@@ -18,18 +30,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={addToCount}>+</button>
+        <button onClick={removeFromCount}>- </button>
+        <p>{count}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
